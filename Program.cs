55e61e8dda54b4b48ea9df444f2a8efb205bbbd4
zhidan <39958842+saodan@users.noreply.github.com>
2019-09-16 15:29:38 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 随机数四则运算
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            Sui s = new Sui(len);
+            //foreach (int i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+    }
+}
---
+++ 计算.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace 随机数四则运算
+{
+    class JiSuan
+    {
+        public JiSuan(int fh_len, List<int> list, List<int> fh)
+        {
+            Dictionary<int, string> hash = new Dictionary<int, string> { { 1, "+" }, { 2, "-" }, { 3, "*" }, { 4, "/" } };
+            foreach (int i in list)
+                Console.Write(i+" ");
+            Console.WriteLine();
+            foreach (int j in fh)
+                Console.Write( hash[j]+ " ");
+            Console.WriteLine();
+            int len = fh.Count();
+            string ji=null;
+            for(int i=0,j=0;i<len;)
+            {
+                if (j < fh.Count())
+                {
+                    ji += list[i] + hash[fh[j]];
+                    j++;
+                }
+                else
+                {
+                    ji += list[i];
+                    i++;
+                }
+                i++;
+            }
+            ji += list[len];
+            Console.WriteLine(ji);
+            var r = new DataTable().Compute(ji, null);
+            Console.WriteLine(r);
+        }
+    }
+}
